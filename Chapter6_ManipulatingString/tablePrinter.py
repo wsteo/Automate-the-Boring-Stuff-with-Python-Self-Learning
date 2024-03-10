b26d109aa9657192ad5e1289795b0f15bd6166ca
wsteo <35697328+wsteo@users.noreply.github.com>
2024-03-10 23:18:35 +0800

    Refactor code and add 1 test case

--- Chapter6_ManipulatingString/tablePrinter.py
+++ Chapter6_ManipulatingString/tablePrinter.py
@@ -1,22 +1,17 @@
-tableData1 = [['apples', 'oranges', 'cherries', 'banana'],
-             ['Alice', 'Bob', 'Carol', 'David'],
-             ['dogs', 'cats', 'moose', 'goose']]
-
-
-
 def printTable(tableData):
 
     colWidths = [0] * len(tableData)
 
     # Find the longest string in each of the inner list and Store the maximum width of each column as a list of intergers
     for i in range(len(tableData)):
-        for word in tableData[i]:
-            colWidthValue = colWidths[i]
-            if colWidthValue == i:
-                colWidths[i] = len(word)
+        for j in range(len(tableData[i])):
+            longestWordWidth = colWidths[i]
+            wordWidth = tableData[i][j]
+            if longestWordWidth == i:
+                colWidths[i] = len(wordWidth)
             else:
-                if colWidthValue < len(word):
-                    colWidths[i] = len(word)
+                if longestWordWidth < len(wordWidth):
+                    colWidths[i] = len(wordWidth)
             
     # Generate Table
     outputTable = []
@@ -28,7 +23,11 @@ def printTable(tableData):
         outputCol.clear()
     
     # Rearrange the content in the table for easy display
-    result = [[],[],[],[]]
+    result = []
+
+    for y in range(len(outputTable[0])):
+        result.append([])
+
     for x in range(len(outputTable)): 
         for y in range(len(outputTable[x])):
             result[y].append(outputTable[x][y])
@@ -39,4 +38,14 @@ def printTable(tableData):
             print(result[x][y], end=' ')
         print()
 
+tableData1 = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+
+tableData2 = [['apple', 'oranges', 'cherries', 'banana', 'grape'],
+              ['Alice', 'Bob', 'Carol', 'David', 'Eve'],
+              ['dogs', 'cats', 'moose', 'goose', 'sheep'],
+              ['elephant', 'giraffe', 'lion', 'tiger', 'zebra']]
+
 printTable(tableData1)
+printTable(tableData2)
